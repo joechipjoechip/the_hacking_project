dfe931a0ef82ca1b1e6960218e9ad5c523c73fc4
Lionel O <lionel.orsini@hotmail.fr>
2017-10-11 19:10:19 +0200

    twitter stream listener update

--- semaine2/j3/04-twitter_bot_listen_to_stream.rb
+++ semaine2/j3/04-twitter_bot_listen_to_stream.rb
@@ -10,6 +10,8 @@ hash_keys = JSON.parse(file)
 @client = Twitter::REST::Client.new(hash_keys["config"])
 @stream = Twitter::Streaming::Client.new(hash_keys["config"])
 
+@tweet_nb_limit = 10
+
 
 # send a tweet with command line
 # @client.update('Mon 1er tweet en ligne de commande ... (test)')
@@ -17,30 +19,86 @@ def write(content)
   @client.update(content)  
 end
 
+
 def launch_stream(keyword)
 
+  i = 0
+  output = File.new("output.json", "w")
 
   if keyword.match(/\s/)
     keyword = keyword.gsub(/\s/, ' AND ')
     # binding.pry
   end
 
+  
+  # f.write("1234567890")     #=> 10
+  output.write('{ 
+  "output" :
+    [')
+  
+
+  
   @stream.filter(:track => keyword) do |tweet|
       if tweet.is_a?(Twitter::Tweet)
-        puts tweet.text
+        
+        user = tweet.user.screen_name
+        text = tweet.text.to_s
 
-        # et donc ici, si on veut, on peut écrire un tweet de réponse avec
-        # @client.update(content)  
-        # on a bien sur accès à tweet.user.screen_name etc ...
+        text = text.gsub('"', "'")
 
+        text = text.gsub(/\n|\r/, "--")
+
+        # un ptit retour console histoire de voir
+        puts "#{user} --- said :"
+        puts text
+        puts "------------------------------------"
+
+        line = '
+      {' + '
+        "name" : "' + "#{user}" + '",' + 
+          '
+        "tweet" : "' + "#{text}" + '"' + 
+          '
+      }'
+
+        if i < @tweet_nb_limit
+          
+          if i != @tweet_nb_limit - 1
+            line = line + ','
+          end
+
+          output.write(line)
+
+        else
+          close_output(output)
+        end
+
+        i += 1
       end
   end
 
 end
 
+def close_output(monFile)
+
+  closing_stuffs = "
+    ]
+  }"
+
+  monFile.write(closing_stuffs)
+  monFile.close
+
+  # rescue => e
+  #   puts "oui, il y a ce pb de fermeture de fichier, mais tout fonctionne correctement ..."
+  #   puts "si qq1 a une solution .."
+
+end
+
+
 
 puts "Quelle recherche ? (plusieurs mots : possible, séparés par un espace"
-keyword = gets.chomp.to_s
+# keyword = gets.chomp.to_s
+keyword = "eminem"
 
 launch_stream(keyword)
 
