f718efae74bd6f94030951884faf055df6d177b6
Lionel O <lionel.orsini@hotmail.fr>
2017-10-13 10:59:10 +0200

    timed loop and prompts add

--- semaine2/j4/04-coinmarket-scrapping.rb
+++ semaine2/j4/04-coinmarket-scrapping.rb
@@ -4,8 +4,16 @@ require 'pry'
 
 # variables globales
 @url_de_base = "https://coinmarketcap.com/#EUR"
-
 @selector = 'tbody tr'
+
+@iteration = 0
+
+puts ("Tell the refresh rate you want (secondes [integer])")
+@refresh_rate = gets.chomp.to_i
+
+puts ("Tell how many times maximum (iteration numer [integer])")
+@refresh_limit = gets.chomp.to_i
+
 
 def scrap(url)
   page = Nokogiri::HTML(RestClient.get(url))
@@ -45,6 +53,25 @@ def go_get_price(keysArr, trTab)
   return newHash
 end
 
+def repeat
+  sleep(@refresh_rate)
+  puts scrap(@url_de_base)
+end
 
-# Starting !
-puts scrap(@url_de_base)
+def start
+  @refresh_limit -= 1
+  puts scrap(@url_de_base)
+
+  while @iteration < @refresh_limit
+    repeat
+    @iteration += 1
+  end
+end
+
+
+start
+
+
+
+
+
