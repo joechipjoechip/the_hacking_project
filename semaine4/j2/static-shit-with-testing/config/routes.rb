76907044292c2c1fe2fd3146db855f090b60e3f3
Lionel O <lionel.orsini@hotmail.fr>
2017-10-25 20:33:43 +0200

    big commit rails

---
+++ semaine4/j2/static-shit-with-testing/config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root 'static_pages#index'
+
+  get 'static_pages/index'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/help'
+
+  get '/home', to: 'static_pages#index'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
